304faf51de5d613173a7b6b85f07ceca0a653ac3
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-09-22 21:18:08 +0200

    Update pygame_movement.py

--- pygame_movement.py
+++ pygame_movement.py
@@ -10,11 +10,11 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption(title)
 
 class Entity:
-    def __init__(self, width, height, speed):
+    def __init__(self, width, height, position: pygame.Vector2, speed):
         self.width = width
         self.height = height
-        self.x = (width - width) // 2
-        self.y = (height - height) // 2
+        self.x = position.x
+        self.y = position.y
         self.speed = speed
 
 class Object:
@@ -23,21 +23,26 @@ class Object:
         self.position = position
 
 
-player = Entity(50, 50, 2)
+player_position = pygame.Vector2(320, 10)
+player = Entity(81, 24, player_position, 1)
+
+v2_1 = pygame.Vector2(0, 360)
+v2_2 = pygame.Vector2(720, 360)
 
 running = True
 while running:
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
-        player.x -= player.speed
-    if keys[pygame.K_RIGHT]:
-        player.x += player.speed
-    if keys[pygame.K_UP]:
-        player.y -= player.speed
-    if keys[pygame.K_DOWN]:
-        player.y += player.speed
-
+    if player.x > 0 and player.x < 620:
+        if keys[pygame.K_LEFT]:
+            player.x -= player.speed
+        if keys[pygame.K_RIGHT]:
+            player.x += player.speed
+    else:
+        if player.x == 620:
+            player.x = 615
+        else:
+            player.x = 25
     print(f'x: {player.x}, y: {player.y}')
 
     for event in pygame.event.get():
@@ -46,7 +51,9 @@ while running:
 
     screen.fill((0, 0, 0))
     pygame.draw.rect(screen, (255, 127, 65), (player.x, player.y, player.width, player.height))
+    pygame.draw.line(screen, (255, 0, 0), v2_1, v2_2, 8)
     pygame.display.update()
 
 pygame.quit()
 sys.exit()
+
